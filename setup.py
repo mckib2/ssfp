a6eecf20eaf3ee0304c7f74e60acc0a49a7fe200
mckib2 <nicholas.bgp@gmail.com>
2020-02-01 15:12:13 -0700

    bump version and update dependency version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='ssfp',
-    version='0.3.0',
+    version='0.4.0',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
@@ -15,11 +15,11 @@ setup(
     description='SSFP simulation',
     long_description=open('README.rst').read(),
     install_requires=[
-        "numpy>=1.18.0",
+        "numpy>=1.18.1",
         "matplotlib>=3.1.2",
-        "scikit-image>=0.15.0",
-        "phantominator>=0.5.0",
-        "tqdm>=4.36.1"
+        "scikit-image>=0.16.2",
+        "phantominator>=0.6.1",
+        "tqdm>=4.42.0"
     ],
     python_requires='>=3.6',
 )
